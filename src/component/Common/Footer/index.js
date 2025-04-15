c7189b2b022d5732550e6dc8ce2eee0cfb8380cb
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-04-15 16:01:06 +0900

    feat: refactor footer

--- src/component/Common/Footer/index.js
+++ src/component/Common/Footer/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import logo from '../../../assets/img/logo.png'
+import logo from '../../../assets/img/bh-logo.jpg'
 import payment from '../../../assets/img/common/payment.png'
 import { Link } from 'react-router-dom'
 import Cookie from '../Cookie'
@@ -17,7 +17,7 @@ const FooterData = [
             { linkTitle: "Privacy Policy", link: "/privacy-policy" },
             { linkTitle: "Frequently Questions", link: "/faqs" },
             { linkTitle: "Order Tracking", link: "/order-tracking" },
-            { linkTitle: "Compare", link: "/compare" }
+            // { linkTitle: "Compare", link: "/compare" }
         ]
     },
     {
@@ -26,8 +26,8 @@ const FooterData = [
             { linkTitle: "Cart View One", link: "/cart" },
             { linkTitle: "Cart View Two", link: "/cartTwo" },
             { linkTitle: "Empty Cart", link: "/empty-cart" },
-            { linkTitle: "Checkout View One", link: "/checkout-one" },
-            { linkTitle: "Checkout View Two", link: "/checkout-two" },
+            // { linkTitle: "Checkout View One", link: "/checkout-one" },
+            // { linkTitle: "Checkout View Two", link: "/checkout-two" },
             { linkTitle: "Wishlist", link: "/wishlist" }
         ]
     }
@@ -89,108 +89,163 @@ const Footer = () => {
     }
 
     return (
-        <>
-            <footer id="footer_one">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4 col-md-12 col-sm-12 col-12">
-                            <div className="footer_left_side">
-                                <Link to="/" ><img src={logo} alt="logo" /></Link>
-                                <p>
-                                    <strong>ANDSHOP</strong> is an multi-vendor B2C fast e-commerce company. The company mainly focuses on men,women and children wear,
-                                    but it also offers other apparel, clothes, accessories, shoes, bags and other fashion items.
-                                </p>
-                                <div className="footer_left_side_icon">
-                                    <ul>
-                                        <li>
-                                            <a href="#!"><i className="fa fa-facebook-f"></i></a>
-                                        </li>
-                                        <li>
-                                            <a href="#!"><i className="fa fa-twitter"></i></a>
-                                        </li>
-                                        <li>
-                                            <a href="#!"><i className="fa fa-linkedin"></i></a>
-                                        </li>
-                                        <li>
-                                            <a href="#!"><i className="fa fa-instagram"></i></a>
-                                        </li>
-                                        <li>
-                                            <a href="#!"><i className="fa fa-google"></i></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+      <>
+        <footer id="footer_one">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4 col-md-12 col-sm-12 col-12">
+                <div className="footer_left_side">
+                  <Link to="/">
+                    <img src={logo} alt="logo" />
+                  </Link>
+                  <p>
+                    <strong>BOOKHEAVEN</strong> is a multi-vendor B2C bookstore
+                    platform. The company primarily focuses on offering a wide
+                    selection of books for all age groups — including fiction,
+                    non-fiction, educational, and children’s books. In addition
+                    to books, it also features various reading accessories,
+                    stationery, journals, and literary gifts from a variety of
+                    trusted vendors.
+                  </p>
+                  <div className="footer_left_side_icon">
+                    <ul>
+                      <li>
+                        <a href="#!">
+                          <i className="fa fa-facebook-f"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#!">
+                          <i className="fa fa-twitter"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#!">
+                          <i className="fa fa-linkedin"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#!">
+                          <i className="fa fa-instagram"></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#!">
+                          <i className="fa fa-google"></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 col-sm-12 col-12">
+                {FooterData.slice(0, 1).map((data, index) => (
+                  <div className="footer_one_widget" key={index}>
+                    <h3>{data.title}</h3>
+                    <ul>
+                      {data.links.map((link, index) => (
+                        <li key={index}>
+                          <Link to={link.link}>{link.linkTitle}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+              <div className="col-lg-2 col-md-6 col-sm-12 col-12">
+                {FooterData.slice(1, 2).map((data, index) => (
+                  <div className="footer_one_widget" key={index}>
+                    <h3>{data.title}</h3>
+                    <ul>
+                      {data.links.map((link, index) => (
+                        <li key={index}>
+                          <Link to={link.link}>{link.linkTitle}</Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+              <div className="col-lg-3 col-md-12 col-sm-12 col-12">
+                <div className="footer_one_widget">
+                  <h3>NEWSLETTER</h3>
+                  <div id="mc_embed_signup" className="subscribe-form">
+                    <form
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        Swal.fire(
+                          "Success",
+                          "Thank you for your Subscribtion",
+                          "success"
+                        );
+                        document.querySelector("input[type='email']").value =
+                          "";
+                      }}
+                    >
+                      <div className="mc-form">
+                        <input
+                          className="form-control"
+                          type="email"
+                          placeholder="Your Mail"
+                          name="EMAIL"
+                          defaultValue=""
+                          required
+                        />
+                        <div className="clear">
+                          <button
+                            className="theme-btn-one btn_md"
+                            type="submit"
+                            name="subscribe"
+                            defaultValue=""
+                          >
+                            {" "}
+                            Send Mail
+                          </button>
                         </div>
-                        <div className="col-lg-3 col-md-6 col-sm-12 col-12">
-                            {FooterData.slice(0, 1).map((data, index) => (
-                                <div className="footer_one_widget" key={index}>
-                                    <h3>{data.title}</h3>
-                                    <ul>
-                                        {data.links.map((link, index) => (
-                                            <li key={index}><Link to={link.link}>{link.linkTitle}</Link></li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            ))}
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="go-top active"
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+          >
+            <i className="fa fa-chevron-up"></i>
+            <i className="fa fa-arrow-up"></i>
+          </div>
+        </footer>
 
-                        </div>
-                        <div className="col-lg-2 col-md-6 col-sm-12 col-12">
-                            {FooterData.slice(1, 2).map((data, index) => (
-                                <div className="footer_one_widget" key={index}>
-                                    <h3>{data.title}</h3>
-                                    <ul>
-                                        {data.links.map((link, index) => (
-                                            <li key={index}><Link to={link.link}>{link.linkTitle}</Link></li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            ))}
-                        </div>
-                        <div className="col-lg-3 col-md-12 col-sm-12 col-12">
-                            <div className="footer_one_widget">
-                                <h3>NEWSLETTER</h3>
-                                <div id="mc_embed_signup" className="subscribe-form">
-                                    <form onSubmit={(e) => { e.preventDefault(); Swal.fire('Success', 'Thank you for your Subscribtion', 'success'); document.querySelector("input[type='email']").value = "" }}>
-                                        <div className="mc-form">
-                                            <input className="form-control" type="email" placeholder="Your Mail" name="EMAIL" defaultValue="" required />
-                                            <div className="clear">
-                                                <button className="theme-btn-one btn_md" type="submit" name="subscribe" defaultValue=""> Send Mail</button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <section id="copyright_one">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 col-md-6 col-sm-6 col-12">
+                <div className="copyright_left">
+                  <h6>
+                    © CopyRight 2025 <span>BookHeaven</span>
+                  </h6>
                 </div>
-                <div className="go-top active" onClick={() => { window.scrollTo(0, 0) }}>
-                    <i className="fa fa-chevron-up"></i>
-                    <i className="fa fa-arrow-up"></i>
+              </div>
+              <div className="col-lg-6 col-md-6 col-sm-6 col-12">
+                <div className="copyright_right">
+                  <img src={payment} alt="img" />
                 </div>
-            </footer>
-
-            <section id="copyright_one">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6 col-md-6 col-sm-6 col-12">
-                            <div className="copyright_left">
-                                <h6>© CopyRight 2022 <span>AndShop</span></h6>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 col-md-6 col-sm-6 col-12">
-                            <div className="copyright_right">
-                                <img src={payment} alt="img" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {
-                cookie ? <Cookie accept={acceptCookie} cancel={cancelCookie} /> : null
-            }
-            <NewsletterModal show={promoCenter} stop={stopPromoModal} start={startPromoModal} />
-        </>
-    )
+              </div>
+            </div>
+          </div>
+        </section>
+        {cookie ? <Cookie accept={acceptCookie} cancel={cancelCookie} /> : null}
+        <NewsletterModal
+          show={promoCenter}
+          stop={stopPromoModal}
+          start={startPromoModal}
+        />
+      </>
+    );
 }
 
 export default Footer
