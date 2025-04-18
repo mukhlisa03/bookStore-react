21fb48a1e3046e9644b191e7636d543efe5eed70
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-04-18 19:55:23 +0900

    fix: modify codes

--- src/page/furniture.js
+++ src/page/furniture.js
@@ -7,7 +7,7 @@ import BlackFriday from '../component/Furniture/BlackFriday'
 import PopularProduct from '../component/Furniture/PopularProduct'
 import OurStory from '../component/Furniture/OurStory'
 import Blog from '../component/Furniture/Blog'
-import InstgramSlider from '../component/Common/Instagram'
+// import InstgramSlider from '../component/Common/Instagram'
 import Footer from '../component/Common/Footer'
 const Furniture = () => {
     return (
@@ -20,7 +20,7 @@ const Furniture = () => {
             <PopularProduct />
             <OurStory />
             <Blog />
-            <InstgramSlider container="container" />
+            {/* <InstgramSlider container="container" /> */}
             <Footer />
         </>
     )
