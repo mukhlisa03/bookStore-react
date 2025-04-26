0bec10e04bb59d117ffa154ba004c26927e7f0b2
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-04-26 17:05:04 +0900

    fix: TotalCard is modified

--- src/component/Cart/TotalCart.js
+++ src/component/Cart/TotalCart.js
@@ -1,84 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Swal from "sweetalert2";
 
 const TotalCart = (props) => {
-    let carts = useSelector((state) => state.products.carts);
+  let carts = useSelector((state) => state.products.carts);
 
-    const cartTotal = () => {
-        return carts.reduce(function (total, item) {
-            return total + ((item.quantity || 1) * item.price)
-        }, 0)
-    }
-    return (
-      <>
-        {props.fullGrid ? (
-          <div className="col-lg-12 col-md-12"> 
-            <div className="coupon_code right">
-              <h3>Cart Total</h3>
-              <div className="coupon_inner">
-                <div className="cart_subtotal">
-                  <p>Subtotal</p>
-                  <p className="cart_amount">${cartTotal()}.00</p>
-                </div>
-                <div className="cart_subtotal ">
-                  <p>Shipping</p>
-                  <p className="cart_amount">
-                    <span>Flat Rate:</span> $00
-                  </p>
-                </div>
-                <a href="#!">Calculate shipping</a>
+  const cartTotal = () => {
+    return carts.reduce(function (total, item) {
+      return total + (item.quantity || 1) * item.price;
+    }, 0);
+  };
 
-                <div className="cart_subtotal">
-                  <p>Total</p>
-                  <p className="cart_amount">${cartTotal()}.00</p>
-                </div>
-                <div className="checkout_btn">
-                  <Link
-                    to="/order-success"
-                    className="theme-btn-one btn-black-overlay btn_sm"
-                  >
-                    Proceed to Checkout
-                  </Link>
-                </div>
-              </div>
-            </div>
+  const handleCheckout = (e) => {
+    e.preventDefault(); // Default behaviorni to'xtatish
+    Swal.fire({
+      icon: "success",
+      title: "Successfully shopping!",
+      text: "Your order has been placed.",
+      confirmButtonText: "OK",
+    }).then(() => {
+      // Agar SweetAlertdan OK bosilsa, checkout sahifasiga o'tish
+      window.location.href = props.fullGrid ? "/order-success" : "/";
+    });
+  };
+
+  return (
+    <div
+      className={props.fullGrid ? "col-lg-12 col-md-12" : "col-lg-6 col-md-6"}
+    >
+      <div className="coupon_code right">
+        <h3>Cart Total</h3>
+        <div className="coupon_inner">
+          <div className="cart_subtotal">
+            <p>Subtotal</p>
+            <p className="cart_amount">${cartTotal()}.00</p>
           </div>
-        ) : (
-          <div className="col-lg-6 col-md-6">
-            <div className="coupon_code right">
-              <h3>Cart Total</h3>
-              <div className="coupon_inner">
-                <div className="cart_subtotal">
-                  <p>Subtotal</p>
-                  <p className="cart_amount">${cartTotal()}.00</p>
-                </div>
-                <div className="cart_subtotal ">
-                  <p>Shipping</p>
-                  <p className="cart_amount">
-                    <span>Flat Rate:</span> $00
-                  </p>
-                </div>
-                <a href="#!">Calculate shipping</a>
-
-                <div className="cart_subtotal">
-                  <p>Total</p>
-                  <p className="cart_amount">${cartTotal()}.00</p>
-                </div>
-                <div className="checkout_btn">
-                  <Link
-                    to="/order-success"
-                    className="theme-btn-one btn-black-overlay btn_sm"
-                  >
-                    Proceed to Checkout
-                  </Link>
-                </div>
-              </div>
-            </div>
+          <div className="cart_subtotal ">
+            <p>Shipping</p>
+            <p className="cart_amount">
+              <span>Flat Rate:</span> $00
+            </p>
           </div>
-        )}
-      </>
-    );
-}
+          {/* <a href="#!">Calculate shipping</a> */}
 
-export default TotalCart
+          <div className="cart_subtotal">
+            <p>Total</p>
+            <p className="cart_amount">${cartTotal()}.00</p>
+          </div>
+          <div className="checkout_btn">
+            <Link
+              to="#!"
+              className="theme-btn-one btn-black-overlay btn_sm"
+              onClick={handleCheckout}
+            >
+              Proceed to Checkout
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TotalCart;
