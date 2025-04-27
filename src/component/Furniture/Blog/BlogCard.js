9937c285d6f253d4dc95157d3ff467ec6f034077
mukhlisa03 <mukhlisamakhmaraimova03@gmail.com>
2025-04-27 22:50:17 +0900

    fix: orders and blog post fixed

--- src/component/Furniture/Blog/BlogCard.js
+++ src/component/Furniture/Blog/BlogCard.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 const BlogCard = (props) => {
-    return (
-        <>
-            <div className="col-lg-6 col-md-6 col-sm-12">
-                    <div className="blog_post_wrapper">
-                        <div className="blog_post_img">
-                          <Link to="/shop">
-                              <img src={props.img} alt="img" />
-                          </Link>
-                          <div className="blog_post_date">
-                              <i className="far fa-calendar-alt"></i>
-                              <span>{props.date}</span>
-                          </div>
-                        </div>
-                        <div className="right_block">
-                            <div className="right_side_content">
-                              <h5>{props.heading}</h5>
-                              <p>{props.para}</p>
-                              <Link to="/shop">{props.button}</Link>
-                            </div>
-                        </div>
-                      </div>
-                </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="col-lg-6 col-md-6 col-sm-12">
+        <div className="blog_post_wrapper">
+          <div className="blog_post_img">
+            <img src={props.img} alt="img" />
+            <div className="blog_post_date">
+              <i className="far fa-calendar-alt"></i>
+              <span>{props.date}</span>
+            </div>
+          </div>
+          <div className="right_block">
+            <div className="right_side_content">
+              <h5>{props.heading}</h5>
+              <p>{props.para}</p>
+              <Link to="/p">{props.button}</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default BlogCard
+export default BlogCard;
